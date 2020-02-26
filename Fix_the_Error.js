82982a65a9771d4fb28287138d4d46a5048898db
Ricky Bergman <erikabergman@gmail.com>
2020-02-26 17:42:23 +0000

    solutions to (4-5) and (4-6)

--- Fix_the_Error.js
+++ Fix_the_Error.js
@@ -6,7 +6,20 @@ for (x < 10; x++) {
     console.log(x);
   }
  */
-// fix the for loop
+
 for (var x = 5; x < 10; ++x) {
     console.log(x);
+}
+
+/*
+ * Programming Quiz: Fix the Error 1 (4-6)
+The for loop below has an error. Fix it!
+for (var k = 0 k < 200 k++) {
+  console.log(k);
+}
+ */
+
+
+for (var k = 0; k < 200; k++) {
+  console.log(k);
 }
