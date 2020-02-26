3c309630486a515139664d9c517eb73886479edc
Ricky Bergman <erikabergman@gmail.com>
2020-02-26 05:18:06 +0000

    cleaner code for 99 Bottles and Countdown

--- 99_Bottles_of_Juice.js
+++ 99_Bottles_of_Juice.js
@@ -9,14 +9,6 @@
  *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
  */
 
-* Use the following `while` loop to write out the song "99 bottles of juice".
-* Log the your lyrics to the console.
-*
-* Note
-*   - Each line of the lyrics needs to be logged to the same line.
-*   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
-*/
-
 var num = 99;
 
 while (num >= 1) {
