e3f756b32694969ebd9124627f00ce4b4885c278
Ricky Bergman <erikabergman@gmail.com>
2020-02-27 16:48:28 +0000

    minor changes to syntax

---
+++ Factorials.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ Write a for loop (not a function) that prints out the factorial of 12
+ */
+
+// your code goes here
+
+
+var solution = 12
+for (var x = 1; x < 12; x++) {
+    solution *= x;
+}
+console.log(solution);
+
+
